3ca171c2915293cc7788e7c79c6e55ac2062541b
baocongchen <baocongchen@yahoo.com>
2022-07-31 18:30:03 -0400

    refactored Location Service in services.py

--- modules/location-api/app/udaconnect/schemas.py
+++ modules/location-api/app/udaconnect/schemas.py
@@ -1,7 +1,5 @@
 from app.udaconnect.models import Location
-from geoalchemy2.types import Geometry as GeometryType
 from marshmallow import Schema, fields
-from marshmallow_sqlalchemy.convert import ModelConverter as BaseModelConverter
 
 
 class LocationSchema(Schema):
