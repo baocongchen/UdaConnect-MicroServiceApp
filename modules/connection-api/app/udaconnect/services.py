c442fe1ff34a8a1805c7175395a62b02197cb2a5
baocongchen <baocongchen@yahoo.com>
2022-08-08 15:46:28 -0400

    debugging Connection API

--- modules/connection-api/app/udaconnect/services.py
+++ modules/connection-api/app/udaconnect/services.py
@@ -27,7 +27,7 @@ class ConnectionService:
         # db.session.query(Location).filter(
         url = ('http://localhost:30001/api/locations')
 
-        response = requests.get(url, verify=False, timeout=5).json()
+        response = requests.get(url, verify=False, timeout=None).json()
         # locations: List = response.filter(
         #     Location.person_id == person_id
         # ).filter(Location.creation_time < end_date).filter(
