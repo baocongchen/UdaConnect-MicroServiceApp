a32089fbfcd81d20435a5f704e524d9a384edbb0
baocongchen <baocongchen@yahoo.com>
2022-08-09 14:39:01 -0400

    debugging Connection API

--- modules/connection-api/app/udaconnect/services.py
+++ modules/connection-api/app/udaconnect/services.py
@@ -32,55 +32,55 @@ class ConnectionService:
         # ).filter(Location.creation_time < end_date).filter(
         #     Location.creation_time >= start_date
         # ).all()
-        # locations = [location for location in response if (start_date <= location.creation_time < end_date)]
+        locations = [location for location in response if (start_date <= location.creation_time < end_date)]
         # Cache all users in memory for quick lookup
-        # person_map: Dict[str, Person] = {person.id: person for person in PersonService.retrieve_all()}
+        person_map: Dict[str, Person] = {person.id: person for person in PersonService.retrieve_all()}
 
         # Prepare arguments for queries
-        # data = []
-        # for location in locations:
-        #     data.append(
-        #         {
-        #             "person_id": person_id,
-        #             "longitude": location.longitude,
-        #             "latitude": location.latitude,
-        #             "meters": meters,
-        #             "start_date": start_date.strftime("%Y-%m-%d"),
-        #             "end_date": (end_date + timedelta(days=1)).strftime("%Y-%m-%d"),
-        #         }
-        #     )
+        data = []
+        for location in locations:
+            data.append(
+                {
+                    "person_id": person_id,
+                    "longitude": location.longitude,
+                    "latitude": location.latitude,
+                    "meters": meters,
+                    "start_date": start_date.strftime("%Y-%m-%d"),
+                    "end_date": (end_date + timedelta(days=1)).strftime("%Y-%m-%d"),
+                }
+            )
 
-        # query = text(
-        #     """
-        # SELECT  person_id, id, ST_X(coordinate), ST_Y(coordinate), creation_time
-        # FROM    location
-        # WHERE   ST_DWithin(coordinate::geography,ST_SetSRID(ST_MakePoint(:latitude,:longitude),4326)::geography, :meters)
-        # AND     person_id != :person_id
-        # AND     TO_DATE(:start_date, 'YYYY-MM-DD') <= creation_time
-        # AND     TO_DATE(:end_date, 'YYYY-MM-DD') > creation_time;
-        # """
-        # )
-        # result: List[Connection] = []
-        # for line in tuple(data):
-        #     for (
-        #         exposed_person_id,
-        #         location_id,
-        #         exposed_lat,
-        #         exposed_long,
-        #         exposed_time,
-        #     ) in db.engine.execute(query, **line):
-        #         location = Location(
-        #             id=location_id,
-        #             person_id=exposed_person_id,
-        #             creation_time=exposed_time,
-        #         )
-        #         location.set_wkt_with_coords(exposed_lat, exposed_long)
+        query = text(
+            """
+        SELECT  person_id, id, ST_X(coordinate), ST_Y(coordinate), creation_time
+        FROM    location
+        WHERE   ST_DWithin(coordinate::geography,ST_SetSRID(ST_MakePoint(:latitude,:longitude),4326)::geography, :meters)
+        AND     person_id != :person_id
+        AND     TO_DATE(:start_date, 'YYYY-MM-DD') <= creation_time
+        AND     TO_DATE(:end_date, 'YYYY-MM-DD') > creation_time;
+        """
+        )
+        result: List[Connection] = []
+        for line in tuple(data):
+            for (
+                exposed_person_id,
+                location_id,
+                exposed_lat,
+                exposed_long,
+                exposed_time,
+            ) in db.engine.execute(query, **line):
+                location = Location(
+                    id=location_id,
+                    person_id=exposed_person_id,
+                    creation_time=exposed_time,
+                )
+                location.set_wkt_with_coords(exposed_lat, exposed_long)
 
-        #         result.append(
-        #             Connection(
-        #                 person=person_map[exposed_person_id], location=location,
-        #             )
-        #         )
+                result.append(
+                    Connection(
+                        person=person_map[exposed_person_id], location=location,
+                    )
+                )
         print(response)
         return response
 
